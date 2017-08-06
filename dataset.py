3ba36657e5b95ace1adbac123ad98bfa9dbe59f4
Kotaro Abe <abekoh.bcky@gmail.com>
2017-08-06 16:34:32 +0900

    [update] #6 update dataset.py

--- dataset.py
+++ dataset.py
@@ -47,6 +47,11 @@ class Dataset():
             self.keys_queue_test = self.keys_queue_train[int(len(self.keys_queue_train) * train_rate):]
             self.keys_queue_train = self.keys_queue_train[:int(len(self.keys_queue_train) * train_rate)]
 
+    def set_category_random(self, id_n):
+        self.category_queue = dict()
+        for key in self.h5file.keys():
+            self.category_queue[key] = random.randint(0, id_n - 1)
+
     def shuffle(self, is_test=False):
         if is_test:
             random.shuffle(self.keys_queue_test)
@@ -58,25 +63,25 @@ class Dataset():
             return len(self.keys_queue_test)
         return len(self.keys_queue_train)
 
-    def get_batch(self, batch_i, batch_size, is_test=False):
+    def get_batch(self, batch_i, batch_size, is_test=False, is_cat=False):
         keys_list = list()
         for i in range(batch_i * batch_size, (batch_i + 1) * batch_size):
             if is_test:
                 keys_list.append(self.keys_queue_test[i])
             else:
                 keys_list.append(self.keys_queue_train[i])
-        return self._get(keys_list)
+        return self._get(keys_list, is_cat)
 
-    def get_random(self, batch_size, is_test=False):
+    def get_random(self, batch_size, is_test=False, is_cat=False):
         keys_list = list()
         for i in range(batch_size):
             if is_test:
                 keys_list.append(random.choice(self.keys_queue_test))
             else:
                 keys_list.append(random.choice(self.keys_queue_train))
-        return self._get(keys_list)
+        return self._get(keys_list, is_cat)
 
-    def get_selected(self, labels, is_test=False):
+    def get_selected(self, labels, is_test=False, is_cat=False):
         keys_list = list()
         for label in labels:
             num = ord(label) - 65
@@ -84,26 +89,34 @@ class Dataset():
                 keys_list.append(self.keys_queue_test[num])
             else:
                 keys_list.append(self.keys_queue_train[num])
-        return self._get(keys_list)
+        return self._get(keys_list, is_cat)
 
-    def get_all(self, is_test=False):
+    def get_all(self, is_test=False, is_cat=False):
         if is_test:
             return self.get_batch(0, len(self.key_queue_test), is_test)
-        return self.get_batch(0, len(self.keys_queue_train), is_test)
+        return self.get_batch(0, len(self.keys_queue_train), is_test, is_cat)
 
-    def _get(self, keys_list):
+    def _get(self, keys_list, is_cat=False):
         imgs = np.empty((0, self.img_size[0], self.img_size[1], 1), np.float32)
         labels = list()
+        cats = list()
         for keys in keys_list:
             img = self.h5file[keys[0] + '/imgs'].value[keys[1]]
             img = img[np.newaxis, :]
             imgs = np.append(imgs, img, axis=0)
             labels.append(self.h5file[keys[0] + '/labels'].value[keys[1]])
+            if is_cat:
+                cats.append(self.category_queue[keys[0]])
+        if is_cat:
+            return imgs, labels, cats
         return imgs, labels
 
 
 if __name__ == '__main__':
     # dataset = Dataset('src/fonts_200_caps_256x256_-1,1.h5', 'w', img_size=(256, 256))
     # dataset.load_imgs('../../font_dataset/png/200_256x256')
-    dataset = Dataset('src/arial.h5', 'w', img_size=(256, 256))
-    dataset.load_imgs('../../font_dataset/png/ariel_256x256')
+    # dataset = Dataset('src/arial.h5', 'w', img_size=(256, 256))
+    # dataset.load_imgs('../../font_dataset/png/ariel_256x256')
+    dataset = Dataset('src/fonts_200_caps_256x256.h5', 'r', img_size=(256, 256))
+    dataset.set_category_random(10)
+    print(dataset.category_queue)
