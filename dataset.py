[fix] #3 change normalization scale from [0, 1] to [-1, 1]

dataset.py
@@ -26,7 +26,7 @@ class Dataset():
             for img_path in img_paths:
                 pil_img = Image.open(img_path)
                 np_img = np.asarray(pil_img)
-                np_img = np_img.astype(np.float32) / 255.0
+                np_img = (np_img.astype(np.float32) / 127.5) - 1.
                 np_img = np_img[np.newaxis, :, :, np.newaxis]
                 imgs = np.append(imgs, np_img, axis=0)
                 labels = np.append(labels, os.path.basename(img_path).split('.')[0])
@@ -103,10 +103,7 @@ class Dataset():
 
 
 if __name__ == '__main__':
-    # dataset = Dataset('src/fonts_200new_caps_64x64', 'w', img_size=(64, 64))
-    # dataset.load_imgs('../../font_dataset/png/200new_64x64_maximum/')
-    dataset = Dataset('src/arial.h5', 'r')
-    dataset.set_load_data()
-    for i in range(26):
-        imgs, labels = dataset.get_batch(i, 1)
-        print(labels)
+    # dataset = Dataset('src/fonts_200_caps_256x256_-1,1.h5', 'w', img_size=(256, 256))
+    # dataset.load_imgs('../../font_dataset/png/200_256x256')
+    dataset = Dataset('src/arial.h5', 'w', img_size=(256, 256))
+    dataset.load_imgs('../../font_dataset/png/ariel_256x256')

train_gan.py
@@ -301,7 +301,7 @@ class TrainingFontDesignGAN():
         concatenated_num_img = np.empty((0, self.params.img_size[1] * 2))
         for img_i in range(dst_imgs.shape[0]):
             num_img = np.concatenate((dst_imgs[img_i], generated_imgs[img_i]), axis=1)
-            num_img = num_img * 255
+            num_img = (num_img + 1.) * 127.5
             num_img = np.reshape(num_img, (self.params.img_size[0], self.params.img_size[1] * 2))
             concatenated_num_img = np.concatenate((concatenated_num_img, num_img), axis=0)
             concatenated_num_img = np.reshape(concatenated_num_img, (-1, self.params.img_size[1] * 2))
@@ -381,7 +381,7 @@ if __name__ == '__main__':
         'src': Params({
             'real_h5': 'src/fonts_200_caps_256x256.h5',
             'src_h5': 'src/arial.h5',
-            'cls_weight_h5': 'output_classifier/classifier_weights_20(train=0.936397172634403,test=0.9258828996282528).h5'
+            # 'cls_weight_h5': 'output_classifier/classifier_weights_20(train=0.936397172634403,test=0.9258828996282528).h5'
         }),
         'dst': Params({
             'root': dst_root,