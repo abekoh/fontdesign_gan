05370de629feb7fc788c494002a3bd0b938ec15e
Kotaro Abe <abekoh.bcky@gmail.com>
2017-08-09 13:54:48 +0900

    [update] divide running script

---
+++ run.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from keras.optimizers import Adam
+
+from train_gan import TrainingFontDesignGAN
+from params import Params
+
+if __name__ == '__main__':
+    params = Params(d={
+        'img_size': (256, 256),
+        'img_dim': 1,
+        'font_embedding_n': 200,
+        'char_embedding_n': 26,
+        'epoch_n': 50,
+        'batch_size': 16,
+        'critic_n': 1,
+        # 'early_stopping_n': 10,
+        'save_metrics_graph_interval': 1,
+        'save_metrics_smoothing_graph_interval': 10,
+        'save_imgs_interval': 10,
+        'save_weights_interval': 5,
+        'g': Params({
+            'arch': 'pix2pix',
+            'opt': Adam(lr=0.0002, beta_1=0.5),
+            'loss_weights': [1.]
+        }),
+        'd': Params({
+            'arch': 'pix2pix',
+            'opt': Adam(lr=0.0002, beta_1=0.5),
+            'loss_weights': [1.]
+        }),
+        'fc': Params({
+            'opt': Adam(lr=0.0002, beta_1=0.5),
+            'loss_weights_d': [0.5],
+            'loss_weights_g': [1.]
+        }),
+        # 'c': Params({
+        #     'opt': RMSprop(lr=0.00005),
+        #     'loss_weights': [0.5]
+        # }),
+        'l1': Params({
+            'opt': Adam(lr=0.0002, beta_1=0.5),
+            'loss_weights': [100.]
+        }),
+        # 'v': Params({
+        #     'opt': RMSprop(lr=0.00005),
+        #     'loss_weights': [1.]
+        # }),
+        'e': Params({
+            'opt': Adam(lr=0.0002, beta_1=0.5),
+            'loss_weights': [15.]
+        })
+    })
+
+    str_now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+    dst_root = 'output/' + str_now
+
+    paths = Params({
+        'src': Params({
+            'real_h5': 'src/fonts_200_caps_256x256.h5',
+            'src_h5': 'src/arial.h5',
+            # 'cls_weight_h5': 'output_classifier/classifier_weights_20(train=0.936397172634403,test=0.9258828996282528).h5'
+        }),
+        'dst': Params({
+            'root': dst_root,
+            'tensorboard_log': '{}/tensorboard_log'.format(dst_root),
+            'generated_imgs': '{}/generated_imgs'.format(dst_root),
+            'model_weights': '{}/model_weights'.format(dst_root),
+            'metrics': '{}/metrics'.format(dst_root),
+        })
+    })
+    gan = TrainingFontDesignGAN(params, paths)
+    gan.train()
--- train_gan.py
+++ train_gan.py
@@ -3,7 +3,6 @@ import random
 import numpy as np
 import h5py
 import json
-from datetime import datetime
 from PIL import Image
 import plotly.offline as py
 import plotly.graph_objs as go
@@ -13,13 +12,11 @@ import colorlover as cl
 import tensorflow as tf
 
 from keras.models import Model
-from keras.optimizers import Adam
 from keras.utils import Progbar, to_categorical
 
 import models
 from dataset import Dataset
 from ops import hamming_error_inv
-from params import Params
 
 CAPS = [chr(i) for i in range(65, 65 + 26)]
 
@@ -325,71 +322,3 @@ class TrainingFontDesignGAN():
         h5file.create_dataset('y_metrics', data=self.y_metrics)
         h5file.flush()
         h5file.close()
-
-
-if __name__ == '__main__':
-    params = Params(d={
-        'img_size': (256, 256),
-        'img_dim': 1,
-        'font_embedding_n': 200,
-        'char_embedding_n': 26,
-        'epoch_n': 50,
-        'batch_size': 16,
-        'critic_n': 1,
-        # 'early_stopping_n': 10,
-        'save_metrics_graph_interval': 1,
-        'save_metrics_smoothing_graph_interval': 10,
-        'save_imgs_interval': 10,
-        'save_weights_interval': 5,
-        'g': Params({
-            'arch': 'pix2pix',
-            'opt': Adam(lr=0.0002, beta_1=0.5),
-            'loss_weights': [1.]
-        }),
-        'd': Params({
-            'arch': 'pix2pix',
-            'opt': Adam(lr=0.0002, beta_1=0.5),
-            'loss_weights': [1.]
-        }),
-        'fc': Params({
-            'opt': Adam(lr=0.0002, beta_1=0.5),
-            'loss_weights_d': [0.5],
-            'loss_weights_g': [1.]
-        }),
-        # 'c': Params({
-        #     'opt': RMSprop(lr=0.00005),
-        #     'loss_weights': [0.5]
-        # }),
-        'l1': Params({
-            'opt': Adam(lr=0.0002, beta_1=0.5),
-            'loss_weights': [100.]
-        }),
-        # 'v': Params({
-        #     'opt': RMSprop(lr=0.00005),
-        #     'loss_weights': [1.]
-        # }),
-        'e': Params({
-            'opt': Adam(lr=0.0002, beta_1=0.5),
-            'loss_weights': [15.]
-        })
-    })
-
-    str_now = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
-    dst_root = 'output/' + str_now
-
-    paths = Params({
-        'src': Params({
-            'real_h5': 'src/fonts_200_caps_256x256.h5',
-            'src_h5': 'src/arial.h5',
-            # 'cls_weight_h5': 'output_classifier/classifier_weights_20(train=0.936397172634403,test=0.9258828996282528).h5'
-        }),
-        'dst': Params({
-            'root': dst_root,
-            'tensorboard_log': '{}/tensorboard_log'.format(dst_root),
-            'generated_imgs': '{}/generated_imgs'.format(dst_root),
-            'model_weights': '{}/model_weights'.format(dst_root),
-            'metrics': '{}/metrics'.format(dst_root),
-        })
-    })
-    gan = TrainingFontDesignGAN(params, paths)
-    gan.train()
