5b8f07b97caf90133fb24e1028e92b65839ce714
Kotaro Abe <abekoh.bcky@gmail.com>
2017-11-13 14:21:13 +0900

    [update] coding now

--- dataset.py
+++ dataset.py
@@ -1,40 +1,41 @@
 import os
 import sys
+import random
+import math
+
 import numpy as np
 from PIL import Image
 import h5py
 from glob import glob
-import random
 from tqdm import tqdm
-import math
 
 from utils import concat_imgs
 
 
 class Dataset():
 
-    def __init__(self, h5_path, mode, img_size=(256, 256), is_binary=False, is_mem=False, img_dim=1, is_flip=False):
+    def __init__(self, h5_path, mode, img_width, img_height, img_dim, is_mem=False):
         self.mode = mode
-        self.img_size = img_size
-        self.is_binary = is_binary
-        self.is_mem = is_mem
+        self.img_width = img_width
+        self.img_height = img_height
         self.img_dim = img_dim
-        self.is_flip = is_flip
-        self._get = self._get_from_file
+        self.is_mem = is_mem
 
+        assert mode == 'r' or mode == 'w', 'mode must be \'r\' or \'w\''
         if self.mode == 'r':
             assert os.path.exists(h5_path), 'hdf5 file is not found: {}'.format(h5_path)
             self.h5file = h5py.File(h5_path, mode)
-            # TODO: be more clearly
-            keys = self.h5file.keys()
-            self.data_n = len(keys)
-            for key in keys:
-                self.label_n = len(self.h5file[key + '/labels'].value)
-                break
-            if self.is_mem:
-                self._put_on_mem()
-                # self._set_label_ids()
-                self._get = self._get_from_mem
+            # # TODO: be more clearly
+            # keys = self.h5file.keys()
+            # self.data_n = len(keys)
+            # for key in keys:
+            #     self.label_n = len(self.h5file[key + '/labels'].value)
+            #     break
+            # if self.is_mem:
+            #     self._put_on_mem()
+            #     self._get = self._get_from_mem
+            # else:
+            #     self._get = self._get_from_file
         if self.mode == 'w':
             if os.path.exists(h5_path):
                 while True:
@@ -52,62 +53,38 @@ class Dataset():
             if not os.path.isdir(dir_path):
                 continue
             img_paths = sorted(glob('{}/*.png'.format(dir_path)))
-            if len(img_paths) == 0:
-                print('png images are not found at {}'.format(dir_path))
-                continue
-            imgs = np.empty((len(img_paths), self.img_size[0], self.img_size[1], self.img_dim), dtype=np.float32)
-            labels = np.empty((len(img_paths)), dtype=object)
-            for i, img_path in enumerate(img_paths):
-                pil_img = Image.open(img_path)
-                np_img = np.asarray(pil_img)
-                if self.is_binary:
-                    np_img = (np_img.astype(np.float32) * 2.) - 1.
-                else:
-                    np_img = (np_img.astype(np.float32) / 127.5) - 1.
-                if self.is_flip:
-                    np_img = np_img * -1.
-                np_img = np_img[np.newaxis, :, :, np.newaxis]
-                if self.img_dim == 3:
-                    np_img = np.repeat(np_img, 3, axis=3)
-                imgs[i] = np_img
-                labels[i] = os.path.basename(img_path).split('.')[0]
-            self._save(os.path.basename(dir_path), imgs, labels)
-
-    def load_imgs_mnist(self, src_dir_path):
-        mode_dir_paths = sorted(glob('{}/*'.format(src_dir_path)))
-        for mode_dir_path in tqdm(mode_dir_paths):
-            img_paths = sorted(glob('{}/**/*.png'.format(mode_dir_path), recursive=True))
-            imgs = np.empty((len(img_paths), 28, 28, 1), dtype=np.float32)
-            labels = np.empty((len(img_paths)), dtype=object)
+            imgs = np.empty((len(img_paths), self.img_width, self.img_height, self.img_dim), dtype=np.float32)
+            fontnames = np.empty((len(img_paths)), dtype=object)
             for i, img_path in enumerate(img_paths):
                 pil_img = Image.open(img_path)
                 np_img = np.asarray(pil_img)
                 np_img = (np_img.astype(np.float32) / 127.5) - 1.
                 np_img = np_img[np.newaxis, :, :, np.newaxis]
+                if self.img_dim == 3:
+                    np_img = np.repeat(np_img, 3, axis=3)
                 imgs[i] = np_img
-                labels[i] = img_path.split('/')[-2]
-            self._save(os.path.basename(mode_dir_path), imgs, labels)
+                fontnames[i] = os.path.basename(img_path).split('.')[0]
+            self._save(os.path.basename(dir_path), imgs, fontnames)
 
-    def _save(self, group_name, imgs, labels):
+    def _save(self, group_name, imgs, fontnames):
         self.h5file.create_group(group_name)
         self.h5file.create_dataset(group_name + '/imgs', data=imgs)
-        self.h5file.create_dataset(group_name + '/labels', data=labels, dtype=h5py.special_dtype(vlen=str))
+        self.h5file.create_dataset(group_name + '/fontnames', data=fontnames, dtype=h5py.special_dtype(vlen=str))
         self.h5file.flush()
 
-    def set_load_data(self, train_rate=1., ids=None):
+    def set_load_data(self, train_rate=1.):
         self.keys_queue_train = list()
-        if self.is_mem:
-            iters = enumerate(self.h5file.values())
-        else:
-            iters = self.h5file.items()
-        for x, value in iters:
-            for i in range(len(value['labels'])):
-                if ids is not None and i not in ids:
-                    continue
-                self.keys_queue_train.append((x, i))
+        # if self.is_mem:
+        #     iters = enumerate(self.h5file.values())
+        # else:
+        #     iters = self.h5file.items()
+        for key, val in self.h5file.items():
+            for i in range(len(val['imgs'])):
+                self.keys_queue_train.append((key, i))
         if train_rate != 1.:
             self.keys_queue_test = self.keys_queue_train[int(len(self.keys_queue_train) * train_rate):]
             self.keys_queue_train = self.keys_queue_train[:int(len(self.keys_queue_train) * train_rate)]
+        print(self.keys_queue_test)
 
     # def _set_label_ids(self, key=None):
     #     self.label_ids = dict()
@@ -176,7 +153,7 @@ class Dataset():
         return self.get_batch(0, len(self.keys_queue_train), is_test, is_label)
 
     def _get_from_file(self, keys_list, is_label=True):
-        imgs = np.empty((len(keys_list), self.img_size[0], self.img_size[1], self.img_dim), np.float32)
+        imgs = np.empty((len(keys_list), self.img_width, self.img_height, self.img_dim), np.float32)
         labels = list()
         for i, keys in enumerate(keys_list):
             img = self.h5file[keys[0] + '/imgs'].value[keys[1]]
@@ -188,14 +165,14 @@ class Dataset():
 
     def _put_on_mem(self):
         print('putting data on memory...')
-        self.imgs = np.empty((self.data_n, self.label_n, self.img_size[0], self.img_size[1], self.img_dim), np.float32)
+        self.imgs = np.empty((self.data_n, self.label_n, self.img_width, self.img_height, self.img_dim), np.float32)
         self.labels = np.empty((self.data_n, self.label_n), object)
         for i, key in enumerate(self.h5file.keys()):
             self.imgs[i] = self.h5file[key + '/imgs'].value
             self.labels[i] = self.h5file[key + '/labels'].value
 
     def _get_from_mem(self, keys_list, is_label=True):
-        imgs = np.empty((len(keys_list), self.img_size[0], self.img_size[1], self.img_dim), np.float32)
+        imgs = np.empty((len(keys_list), self.img_width, self.img_height, self.img_dim), np.float32)
         labels = list()
         for i, keys in enumerate(keys_list):
             img = self.imgs[keys[0]][keys[1]]
@@ -212,7 +189,7 @@ class Dataset():
         imgs, _ = self.get_random(64)
         concated_img = concat_imgs(imgs, 8, 8)
         concated_img = (concated_img + 1.) * 127.5
-        concated_img = np.reshape(concated_img, (self.img_size[0] * 8, -1))
+        concated_img = np.reshape(concated_img, (self.img_width * 8, -1))
         pil_img = Image.fromarray(np.uint8(concated_img))
         pil_img.show()
 
@@ -222,6 +199,12 @@ class Dataset():
         col_n = math.ceil(math.sqrt(imgs_n))
         concated_img = concat_imgs(imgs, col_n, col_n)
         concated_img = (concated_img + 1.) * 127.5
-        concated_img = np.reshape(concated_img, (-1, col_n * self.img_size[0], self.img_dim))
+        concated_img = np.reshape(concated_img, (-1, col_n * self.img_width, self.img_dim))
         pil_img = Image.fromarray(np.uint8(concated_img))
         pil_img.save(dst_img_path)
+
+
+if __name__ == '__main__':
+    dataset = Dataset('./src/200_64x64.h5', 'r', 64, 64, 3, is_mem=True)
+    # dataset.load_imgs('../../font_dataset/200_64x64_by_char')
+    dataset.set_load_data(0.9)
