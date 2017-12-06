61647b414d62f822b36449adfd677c173996aa23
Kotaro Abe <abe@human.ait.kyushu-u.ac.jp>
2017-12-06 15:07:50 +0900

    [update] pseudo hamming distance

--- evaluate.py
+++ evaluate.py
@@ -1,8 +1,10 @@
 import os
 import csv
+import sys
 
 import tensorflow as tf
 import numpy as np
+import cv2
 from tqdm import tqdm
 
 from dataset import Dataset
@@ -37,24 +39,38 @@ class Evaluating():
         self.char_embedding_n = len(self.embedding_chars)
 
     def calc_hamming_distance(self):
+        def transform_backgorund_distance(src_imgs):
+            bin_imgs = np.clip(-src_imgs, 0., 1.).astype(np.uint8)
+            src_imgs = ((src_imgs + 1.) * 127.5).astype(np.uint8)
+            dist_imgs = np.empty(src_imgs.shape)
+            img_n = src_imgs.shape[0]
+            for i in range(img_n):
+                dist_imgs[i] = cv2.distanceTransform(src_imgs[i], cv2.DIST_L2, 3)
+            return bin_imgs, dist_imgs
+
         def plot(distances, filename):
             fig = plt.figure(figsize=(16, 9))
             ax = fig.add_subplot(1, 1, 1)
-            ax.hist(distances, bins=50, range=(0, 2000))
+            ax.hist(distances, bins=50)
             plt.savefig(os.path.join(self.dst_evaluated, '{}.png'.format(filename)))
             plt.close()
 
         min_distances_list = list()
         for c in tqdm(self.embedding_chars):
             generated_n = self.generated_dataset.get_data_n_by_labels([c])
-            real_n = self.real_dataset.get_data_n_by_labels([c])
             generated_imgs = np.mean(self.generated_dataset.get_batch_by_labels(0, generated_n, [c]), axis=3)
+            bin_generated_imgs, dist_generated_imgs = transform_backgorund_distance(generated_imgs)
+            del generated_imgs
+            real_n = self.real_dataset.get_data_n_by_labels([c])
             real_imgs = np.mean(self.real_dataset.get_batch_by_labels(0, real_n, [c]), axis=3)
+            bin_real_imgs, dist_real_imgs = transform_backgorund_distance(real_imgs)
+            del real_imgs
             min_distances = list()
             for generated_i in tqdm(range(generated_n)):
                 min_distance = float('inf')
                 for real_i in range(real_n):
-                    distance = np.sum(np.absolute(real_imgs[real_i] - generated_imgs[generated_i]))
+                    distance = np.sum(np.multiply(dist_generated_imgs[generated_i], bin_real_imgs[real_i]) +
+                                      np.multiply(dist_real_imgs[generated_i], bin_generated_imgs[generated_i]))
                     min_distance = min(min_distance, distance)
                 min_distances.append(min_distance)
             plot(min_distances, c)
